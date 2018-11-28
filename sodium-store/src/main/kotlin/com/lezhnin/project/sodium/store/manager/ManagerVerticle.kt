7f2e75d71592900536296c5417e0fd57508c4ab7
sbt-lezhnin-sa <salezhnin.sbt@sberbank.ru>
2018-11-28 10:56:32 +0500

    Revert "add value path handling endpoint and service"
    This reverts commit 219f44ca

--- sodium-store/src/main/kotlin/com/lezhnin/project/sodium/store/manager/ManagerVerticle.kt
+++ sodium-store/src/main/kotlin/com/lezhnin/project/sodium/store/manager/ManagerVerticle.kt
@@ -10,28 +10,14 @@ class ManagerVerticle : AbstractVerticle() {
     override fun start() {
         val logger = LoggerFactory.getLogger(ManagerVerticle::class.java)!!
         val router = Router.router(getVertx())
-        val dataForKeyService = DataForKeyService(logger)
-        val dataForPathService = DataForPathService(dataForKeyService, logger)
 
         router
-            .route(
-                "${Web.PATH}:${Web.PARAMETER}"
-            )
-            .handler(
-                ManagerDataRequestHandler(
-                    config().getString(Sodium.MAP_NAME, Sodium.DEFAULT_MAP_NAME),
-                    dataForKeyService,
-                    logger
-                )
-            )
-        router
             .routeWithRegex(
-                "${Web.PATH.replace("/", "\\/")}(?<${Web.PARAMETER}>[^\\/]+)/(?<${Web.VALUE_PATH}>.+)"
+                "${Web.PATH.replace("/", "\\/")}(?<${Web.PARAMETER}>[^\\/]+)(?<${Web.VALUE_PATH}>[^\\?]+)"
             )
             .handler(
                 ManagerDataRequestHandler(
                     config().getString(Sodium.MAP_NAME, Sodium.DEFAULT_MAP_NAME),
-                    dataForPathService,
                     logger
                 )
             )
